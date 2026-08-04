186c766f3523caee6676c4f7477c89bbf885c1c4
PadjokeJ <jonatan@padjokej.dev>
2026-08-04 16:49:16 +0200

    feat: author creators

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::{collections::HashMap, fmt::Display};
+use std::{collections::HashMap, fmt::Display, hash::Hash};
 
 use regex::regex;
 
 use std::fs::File;
 use std::io::prelude::*;
 
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, Serializer};
 
 #[derive(Serialize, Deserialize, Hash, PartialEq, Eq, Debug)]
 enum Loader {
@@ -27,15 +27,33 @@ impl Loader {
     }
 }
 
-#[derive(Serialize, Deserialize, Hash, PartialEq, Eq, Debug)]
+#[derive(Deserialize, Hash, PartialEq, Eq, Debug)]
 enum Version {
     Release(String),
     Pre(String),
     Candidate(String),
     Snapshot(String),
-    Beta(String),
     Alpha(String),
+    Beta(String),
     Other(String),
+}
+
+impl Serialize for Version {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let string = match self {
+            Self::Release(s) => s,
+            Self::Pre(s) => s,
+            Self::Candidate(s) => s,
+            Self::Snapshot(s) => s,
+            Self::Alpha(s) => s,
+            Self::Beta(s) => s,
+            Self::Other(s) => s,
+        };
+        serializer.serialize_str(&string)
+    }
 }
 
 impl Version {
@@ -55,12 +73,12 @@ impl Version {
         regex!(r"^[1-2]\dw[0-5]\d[a-f]$|2[6-9]\.[1-4]-snapshot-\d*").is_match(s)
     }
 
-    fn is_beta(s: &str) -> bool {
-        regex!(r"^b1\.[0-8](\.1?\d)?(_\d*|[a-b])?$").is_match(s)
-    }
-
     fn is_alpha(s: &str) -> bool {
         regex!(r"^a1\.[0-2].1?\d(_\d*|[a-b])?$").is_match(s)
+    }
+
+    fn is_beta(s: &str) -> bool {
+        regex!(r"^b1\.[0-8](\.1?\d)?(_\d*|[a-b])?$").is_match(s)
     }
 
     fn from_str(s: &str) -> Self {
@@ -87,6 +105,7 @@ struct Data {
     licenses: HashMap<String, i32>,
     versions: HashMap<Version, i32>,
     loaders: HashMap<Loader, i32>,
+    authors: HashMap<String, i32>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -131,6 +150,7 @@ fn main() {
         licenses: HashMap::new(),
         versions: HashMap::new(),
         loaders: HashMap::new(),
+        authors: HashMap::new(),
     };
 
     let entries = std::fs::read_dir("results").unwrap();
@@ -148,6 +168,13 @@ fn main() {
                     let license = hit.license;
                     let versions = hit.versions;
                     let categories = hit.categories;
+                    let authors = match hit.author {
+                        Some(a) => a,
+                        None => match hit.organization {
+                            Some(org) => org,
+                            None => "null".to_string(),
+                        },
+                    };
 
                     for category in categories {
                         match Loader::from_str(&category) {
@@ -157,8 +184,13 @@ fn main() {
                     }
 
                     for version in versions {
-                        *data.versions.entry(Version::from_str(&version)).or_insert(0) += 1;
+                        *data
+                            .versions
+                            .entry(Version::from_str(&version))
+                            .or_insert(0) += 1;
                     }
+
+                    *data.authors.entry(authors).or_insert(0) += 1;
 
                     *data.licenses.entry(license).or_insert(0) += 1;
                 }
@@ -169,39 +201,36 @@ fn main() {
 
     let mut file = File::create("data.json").unwrap();
 
-    /*file.write_all(serde_json::to_string(&data).unwrap().as_bytes())
-        .unwrap();*/
+    file.write_all(serde_json::to_string(&data).unwrap().as_bytes())
+        .unwrap();
 
     let max_length = 90;
 
     let mut max_size = 0;
 
-    for i in &data.versions {
-        match i.0 {
-            Version::Alpha(_) => (),
-            _ => continue,
-        };
+    for i in &data.authors {
         if *i.1 > max_size {
             max_size = *i.1;
         }
     }
 
     let divisor = (max_size as f32 / max_length as f32) as i32;
-    let mut hash_vec: Vec<_> = data.versions.iter().collect();
+    let mut hash_vec: Vec<_> = data.authors.iter().collect();
     hash_vec.sort_by(|a, b| b.1.cmp(a.1));
 
+    let mut limit = 100;
     for i in hash_vec {
-        match i.0 {
-            Version::Alpha(_) => (),
-            _ => continue,
-        };
         let l = format!("{:?} ({})", i.0, i.1).len();
         println!(
-            "{:?} ({}){}:  {}",
+            "{} ({}){}:  {}",
             i.0,
             i.1,
             " ".repeat(40 - l),
             "#".repeat((*i.1 / divisor) as usize + 1)
         );
+        limit -= 1;
+        if limit < 0 {
+            break;
+        }
     }
 }
